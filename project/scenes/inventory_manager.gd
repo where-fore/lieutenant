6e4856e1cb86f16757b9418f6b9fe3efb7d9d403
wherefore <wherefore0@gmail.com>
2026-02-27 10:23:09 -0500

    clean up
    testing as an export bool. sprite updates on function not on process, bug fix

--- project/scenes/inventory_manager.gd
+++ project/scenes/inventory_manager.gd
@@ -2,12 +2,14 @@ extends GridContainer
 
 var inventory_slots : Array[Node] = []
 
-@export var test_item:ItemData
+@export var test_item: ItemData
+@export var should_test: bool
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	find_inventory_slots()
-	#equip(test_item)
+	if should_test: equip(test_item)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,14 +24,15 @@ func find_inventory_slots():
 		inventory_slots.append(node as InventorySlot)
 
 
-#returns an InventorySlot class if it finds one, or null if none.
+#returns an InventorySlot class if it finds one empty, or null if none available
 func find_first_empty_slot():
 	for slot: InventorySlot in inventory_slots:
 		if slot.is_empty(): return slot
 	#if we get this far, ie. iterated through all slots
 	return null
 
+
 func equip(item_to_equip:ItemData):
-	var slot_to_equip_to = find_first_empty_slot()
-	if slot_to_equip_to: slot_to_equip_to.equip(item_to_equip)
+	var slot_to_equip_to: InventorySlot = find_first_empty_slot()
+	if slot_to_equip_to: slot_to_equip_to.equip_item(item_to_equip)
 	else: print_debug("inventory full")
--- project/scripts/inventory_slot.gd
+++ project/scripts/inventory_slot.gd
@@ -3,19 +3,22 @@ class_name InventorySlot
 
 @export var item_in_slot: ItemData
 
-@onready var slot_sprite = $"Inventory Item"
+@onready var slot_sprite = $"ItemSprite"
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if item_in_slot:
-		slot_sprite.texture = item_in_slot.item_sprite
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	update_sprite()
 
 
 func equip_item(item_to_equip):
 	item_in_slot = item_to_equip
+	update_sprite()
 
 
 func is_empty() -> bool:
 	return item_in_slot == null
+
+
+func update_sprite():
+	if item_in_slot: slot_sprite.texture = item_in_slot.item_sprite
