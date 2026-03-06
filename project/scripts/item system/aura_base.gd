8dc9c942777a0f7d8904b81c3682a151bce832ea
wherefore <wherefore0@gmail.com>
2026-03-06 11:49:36 -0500

    auras more boilerplate

--- project/scripts/item system/aura_base.gd
+++ project/scripts/item system/aura_base.gd
@@ -1,13 +1,26 @@
 extends Resource
 class_name Aura
 
+@export var health: int = 0
+@export var attack: int = 0
+@export var health_multiplier: int = 0
+
 var unique_id: String
 var additive_dictionary:Dictionary[StringName, int] = {}
 var multiplicative_dictionary:Dictionary[StringName, int] = {}
 
+#this is currently a hacky implementation, awaiting json conversion
+#this setup_aura_stats gets called externally by item_data
+#overriding this with a custom aura would make it so it ignores the export variables
 #derived subclasses hook onto these functions
 func setup_aura_stats() -> void:
+	add_exports_to_dictionaries()
 	pass
+
+func add_exports_to_dictionaries() -> void:
+	if health: additive_dictionary[Stats.health] = health
+	if attack: additive_dictionary[Stats.attack] = attack
+	if health_multiplier: multiplicative_dictionary[Stats.health] = health_multiplier
 
 func _init() -> void:
 	unique_id = str(self.get_instance_id()) + "_" + str(Time.get_time_string_from_system())
--- project/scripts/item system/items/paladin's armor/paladins_might.gd
+++
@@ -1,6 +0,0 @@
-extends Aura
-
-func setup_aura_stats() -> void:
-	additive_dictionary[Stats.health] = +5
-	additive_dictionary[Stats.attack] = +2
-	multiplicative_dictionary[Stats.health] = +1
