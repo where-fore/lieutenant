1f09e9a6a50760ef19b4a6dcb241f9ea05b6ba32
wherefore <wherefore0@gmail.com>
2026-03-16 09:14:21 -0400

    settings menu

--- project/scripts/managers/main.gd
+++ project/scripts/managers/main.gd
@@ -1,26 +1,38 @@
 extends Node2D
 
-@export var main_rpg_game_scene:PackedScene
+@export var main_rpg_scene:PackedScene
 @onready var level_container:Node2D = $LevelContainer
 @onready var game_start_button_container:CanvasLayer = $GameStarter
+@onready var pause_menu_button_container:CanvasLayer = $PauseMenuButton
+@onready var pause_menu_container:CanvasLayer = $PauseMenu
+
+
 
 func _ready() -> void:
 	game_start_button_container.visible = true
+	pause_menu_button_container.visible = true
+	pause_menu_container.visible = false
 
-func start_rpg_game() -> void:
+
+func open_pause_menu() -> void:
+	pass
+
+func start_rpg() -> void:
 	game_start_button_container.visible = false
 	
-	clear_old_level(level_container)
-	var new_level:Node = main_rpg_game_scene.instantiate()
+	for child:Node in level_container.get_children():
+		child.queue_free()
+	var new_level:Node = main_rpg_scene.instantiate()
 	level_container.add_child(new_level)
 
-func clear_old_level(parent_node:Node) -> void:
-	for child:Node in parent_node.get_children():
-		child.queue_free()
-
-
 func _on_new_game_button_pressed() -> void:
-	start_rpg_game()
+	start_rpg()
 
 func _on_load_game_button_pressed() -> void:
-	start_rpg_game()
+	start_rpg()
+
+func _on_pause_menu_button_pressed() -> void:
+	if not pause_menu_container.visible:
+		pause_menu_container.visible = true
+	else:
+		pause_menu_container.visible = false
