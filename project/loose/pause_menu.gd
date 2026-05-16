1985afff4f4d0bd04f866128ad69b1787ca19591
wherefore <wherefore0@gmail.com>
2026-05-16 16:25:37 -0400

    cleaned up the math info display
    made it an actual button and a scroll window, instead of a huge tooltip

--- project/loose/pause_menu.gd
+++ project/loose/pause_menu.gd
@@ -1,4 +1,15 @@
 extends Control
 
+@onready var math_info_panel:Control = $MathInfoPanel
+
+func _ready() -> void:
+	math_info_panel.visible = false
+
 func _on_save_game_button_pressed() -> void:
 	SaveManager.save_game()
+
+func _on_info_button_pressed() -> void:
+	math_info_panel.visible = true
+
+func _on_close_button_pressed() -> void:
+	math_info_panel.visible = false
