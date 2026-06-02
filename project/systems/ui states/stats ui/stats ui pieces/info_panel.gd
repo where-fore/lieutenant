d2b5526cb30521a206fb5105778641ccb7a6fcd6
wherefore <wherefore0@gmail.com>
2026-06-02 09:46:00 -0400

    fortitude on ui
    also stat ui tooltips show what you're currently gaining from stats

--- project/systems/ui states/stats ui/stats ui pieces/info_panel.gd
+++ project/systems/ui states/stats ui/stats ui pieces/info_panel.gd
@@ -11,6 +11,7 @@ class_name InfoPanel
 @export var my_strength:DisplayStat
 @export var my_agility:DisplayStat
 @export var my_mind:DisplayStat
+@export var my_fortitude:DisplayStat
 
 var my_combatant:Combatant
 
@@ -24,12 +25,22 @@ func update_title_stats() -> void:
 	my_health_label.text = str(my_combatant.current_stats.get(Stats.health, 0))
 	my_attack_label.text = str(my_combatant.current_stats.get(Stats.attack, 0))
 	
-	my_strength.my_label.text = str(my_combatant.current_stats.get(Stats.strength, 0))
-	my_strength.tooltip_text = str(Stats.strength_per_attack) + " strength = 1 attack" + "\n" + str(Stats.strength_per_health) + " strength = 1 health"
-	my_strength.tooltip_text = str(Stats.strength_per_attack, " ", Stats.strength, " = 1 attack", "\n", + Stats.strength_per_health, " ", Stats.strength, " = 1 health")
+	var current_strength:int = my_combatant.current_stats.get(Stats.strength, 0)
+	my_strength.my_label.text = str(current_strength)
+	my_strength.tooltip_text = str("Attack increased by ", current_strength/Stats.strength_per_attack, ", Health increased by ", current_strength/Stats.strength_per_health)
+	my_strength.tooltip_text += str("\n", Stats.strength_per_attack, " ", Stats.strength, " = 1 attack", "\n", + Stats.strength_per_health, " ", Stats.strength, " = 1 health")
 	
-	my_agility.my_label.text = str(my_combatant.current_stats.get(Stats.agility, 0))
-	my_agility.tooltip_text = str(Stats.agility_per_attack, " ", Stats.agility, " = 1 attack", "\n", + Stats.agility_per_health, " ", Stats.agility, " = 1 health")
+	var current_agility:int = my_combatant.current_stats.get(Stats.agility, 0)
+	my_agility.my_label.text = str(current_agility)
+	my_agility.tooltip_text = str("Attack increased by ", current_agility/Stats.agility_per_attack, ", Health increased by ", current_agility/Stats.agility_per_health)
+	my_agility.tooltip_text += str("\n", Stats.agility_per_attack, " ", Stats.agility, " = 1 attack", "\n", + Stats.agility_per_health, " ", Stats.agility, " = 1 health")
 	
-	my_mind.my_label.text = str(my_combatant.current_stats.get(Stats.mind, 0))
-	my_mind.tooltip_text = str(Stats.mind_per_shield_per_turn, " ", Stats.mind, " = 1 shield per turn")
+	var current_mind:int = my_combatant.current_stats.get(Stats.mind, 0)
+	my_mind.my_label.text = str(current_mind)
+	my_mind.tooltip_text = str("Shielding every turn for ", current_mind/Stats.mind_per_shield_per_turn)
+	my_mind.tooltip_text += str("\n", Stats.mind_per_shield_per_turn, " ", Stats.mind, " = 1 shield per turn")
+	
+	var current_fortitude:int = my_combatant.current_stats.get(Stats.fortitude, 0)
+	my_fortitude.my_label.text = str(current_fortitude)
+	my_fortitude.tooltip_text = str("Health increased by ", current_fortitude/Stats.fortitude_per_health)
+	my_fortitude.tooltip_text += str("\n", Stats.fortitude_per_health, " ", Stats.fortitude, " = 1 health")
--- project/z individual pieces/combatants/basic_knight_2.gd
+++ project/z individual pieces/combatants/basic_knight_2.gd
@@ -5,8 +5,8 @@ func _init() -> void:
 	combatant_texture = load("res://sprites/knight.png")
 	extra_tooltip = "" # "Generic flavourful description"
 	
-	starting_stats[Stats.strength] = BalanceData.player_base_strength
-	starting_stats[Stats.agility] = BalanceData.player_base_agility / 2
+	starting_stats[Stats.strength] = BalanceData.player_base_strength * 1 / 2
+	starting_stats[Stats.agility] = BalanceData.player_base_agility * 3 / 2
 	starting_stats[Stats.fortitude] = BalanceData.player_base_fortitude
 
 	starting_items = [Database.get_reward_by_id("enrage_sword")]
