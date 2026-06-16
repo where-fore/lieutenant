5ec41b9f6cd12137b633712082672a74b7f91b31
wherefore <wherefore0@gmail.com>
2026-06-16 17:15:13 -0400

    tutorial stats a bit better
    first reward is ezpz, second reward is choice between any stat (random flowers)

--- project/z individual pieces/map tiles/tutorial_second_combat.gd
+++ project/z individual pieces/map tiles/tutorial_second_combat.gd
@@ -3,5 +3,17 @@ extends MapTileData
 func _init() -> void:
 	tile_animation = load("res://z individual pieces/map tiles/animations/forest_basic.tres")
 	enemies = [Database.get_combatant_by_id("tutorial_wolf"), Database.get_combatant_by_id("tutorial_wolf")]
-	rewards = [Database.get_rewards_by_category(Categories.item_rarity, [Categories.Rarity.COMMON]).pick_random()]
-	basic_reward = true
+	rewards = pick_rewards()
+	basic_reward = false
+
+func pick_rewards() -> Array[Reward]:
+	var number_of_rewards:int = 3
+	
+	var possible_rewards:Array[Reward] = Database.get_rewards_by_category(Categories.aura_rarity, [Categories.AuraRarity.BASIC_STAT])
+	possible_rewards.shuffle()
+	
+	var chosen_rewards:Array[Reward]
+	for i:int in number_of_rewards:
+		chosen_rewards.append(possible_rewards.pop_front())
+	
+	return chosen_rewards
